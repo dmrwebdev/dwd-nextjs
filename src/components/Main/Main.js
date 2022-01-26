03920ac65ce10259f5674d910b082570fbcd1f68
Derek M. Robertson <dmr.webdev@gmail.com>
2022-01-25 17:32:32 -0800

    Removed unneeded files

--- src/components/Main/Main.js
+++
@@ -1,9 +0,0 @@
-export default function Main({ children }) {
-  return (
-    <main className="w-full relative z-10 ">
-      <div className="w-full max-w-[1600px] mx-auto overflow-hidden rounded-3xl sm:border ">
-        {children}
-      </div>
-    </main>
-  );
-}
