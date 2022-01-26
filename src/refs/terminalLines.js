68bca5af1a0562d7cca4a3361422531b8d042a59
Derek M. Robertson <dmr.webdev@gmail.com>
2022-01-25 17:35:09 -0800

    New ref for terminal

---
+++ src/refs/terminalLines.js
@@ -0,0 +1,59 @@
+const date = new Date();
+
+const constructedDate = new Intl.DateTimeFormat("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+}).format(new Date());
+
+const dateString = `${constructedDate} ${date.getUTCHours()}:${date.getUTCMinutes()}:${
+  date.getUTCSeconds() < 10 ? `0${date.getUTCSeconds()}` : date.getUTCSeconds()
+} UTC ${date.getUTCFullYear()}`;
+
+const terminalLines = [
+  ["derek@DMR-DESKTOP", "~", "ssh-dmrServer"],
+  /* ["_", "", ""], */
+  [
+    "",
+    "",
+    `Welcome to Ubuntu 20.04.2 LTS (GNU/Linux 5.4.0-91-generic x86_64)
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/advantage
+
+  System information as of ${dateString.replace(",", "")}
+
+  System load:           0.05
+  Usage of /:            45.1% of 24.06GB
+  Memory usage:          86%
+  Swap usage:            0%
+  Processes:             124
+  Users logged in:       0
+  IPv4 address for eth0: 161.230.121.193
+  IPv4 address for eth0: 10.34.0.1
+  IPv6 address for eth0: 2203:a820:4:3d0::180:e000
+  IPv4 address for eth1: 10.120.0.3
+
+4 updates can be applied immediately.
+0 of these updates are standard security updates.
+To see these additional updates run: apt list --upgradable
+
+
+*** System restart required ***
+Last login: Fri Jan 14 06:50:12 2022 from 70.100.60.100
+    `,
+    true,
+  ],
+  ["derek@DMR-WEBDEV", "~", "cd projects/react/dwd"],
+  ["derek@DMR-WEBDEV", "~/projects/react/dwd", "yarn start"],
+  ["", "", "yarn run v1.22.10", true],
+  /*     
+    ["", "", "wait  - compiling...", true],
+    [
+      "",
+      "",
+      "ready - started server on 0.0.0.0:3000, url: http://localhost:3000",
+    ], */
+];
+
+export default terminalLines;
