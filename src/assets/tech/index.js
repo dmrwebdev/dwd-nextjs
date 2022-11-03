9eea5f7f2c9beabb8391c2fc728e8e1671bb4248
Derek M. Robertson <dmr.webdev@gmail.com>
2022-11-03 16:34:18 -0700

    Export tech images

---
+++ src/assets/tech/index.js
@@ -0,0 +1,61 @@
+import apache from "./apache.svg";
+import aws from "./aws.svg";
+import bootstrap from "./bootstrap.svg";
+import digitalOcean from "./digital_ocean.svg";
+import docker from "./docker.svg";
+import express from "./express.webp";
+import git from "./git.svg";
+import go from "./go.svg";
+import graphql from "./graphql.svg";
+import javascript from "./javascript.svg";
+import jQuery from "./jquery.svg";
+import kubernetes from "./kubernetes.svg";
+import linux from "./tux.svg";
+import mongoDB from "./mongodb.svg";
+import mongoose from "./mongoose.png";
+import mysql from "./mysql.svg";
+import nextjs from "./nextjs.svg";
+import nginx from "./nginx.svg";
+import nodejs from "./nodejs.svg";
+import postgres from "./postgres.svg";
+import python from "./python.svg";
+import rails from "./rails.svg";
+import ruby from "./ruby.svg";
+import react from "./react.svg";
+import sass from "./sass.svg";
+import sequelize from "./sequelize.svg";
+import sql from "./sql.svg";
+import tailwind from "./tailwindcss.svg";
+import typescript from "./typescript.svg";
+
+export {
+  apache,
+  aws,
+  bootstrap,
+  digitalOcean,
+  docker,
+  express,
+  git,
+  go,
+  graphql,
+  javascript,
+  jQuery,
+  kubernetes,
+  linux,
+  mongoDB,
+  mongoose,
+  mysql,
+  nextjs,
+  nginx,
+  nodejs,
+  postgres,
+  python,
+  rails,
+  ruby,
+  react,
+  sass,
+  sequelize,
+  sql,
+  tailwind,
+  typescript,
+};
